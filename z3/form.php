0073eab2defd424d04ac3fb3ca85089a525fb8fd
ArtN7 <112850344+ArtN7@users.noreply.github.com>
2023-03-14 17:15:49 +0300

    Update form.php

--- z3/form.php
+++ z3/form.php
@@ -2,6 +2,7 @@
   <input name="fio" placeholder="fio"/>
   <input name="mail" placeholder="mail">
   <input name="year" placeholder="year">
-  
+  <input name="gender" type="radio" value="man">
+  <input name="gender" type="radio" value="woman">
   <input type="submit" value="ok" />
 </form>
