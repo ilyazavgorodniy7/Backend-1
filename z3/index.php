ab9cfcce448a47643a9f9190de7c9293e69dba4e
ArtN7 <112850344+ArtN7@users.noreply.github.com>
2023-03-14 21:47:28 +0300

    Update index.php

--- z3/index.php
+++ z3/index.php
@@ -76,7 +76,7 @@ catch(PDOException $e){
 
 foreach ($_POST['abilities'] as $ability) {
   $stmt = $db->prepare("INSERT INTO abilities SET ability= ?");
-  $stmt->execute([$_POST['ability']);
+  $stmt->execute([$_POST['ability']]);
 }
 //  stmt - это "дескриптор состояния".
  
