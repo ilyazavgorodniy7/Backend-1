4f5208acd05e496412793b618b9efde711da535d
ArtN7 <112850344+ArtN7@users.noreply.github.com>
2023-03-14 17:24:37 +0300

    Update form.php

--- z3/form.php
+++ z3/form.php
@@ -18,13 +18,19 @@
   </br>
   <label>
     <?php
-    printf('Кол-во конечностей');
+      printf('Кол-во конечностей');
+    ?>
     <input name="count_limb" type="radio" value="2-4">
-    printf('2-4');
+    <?php
+      printf('2-4');
+    ?>
     <input name="count_limb" type="radio" value="5-7">
-    printf('5-7');
+    <?php
+      printf('5-7');
+    ?>
     <input name="count_limb" type="radio" value="8-10">
-    printf('8-10');
+    <?php
+      printf('8-10');
     ?>
    </label>
    
