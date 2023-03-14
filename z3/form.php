6c838fd891fcaea5d8d74fd3230eb0279a22c01a
ArtN7 <112850344+ArtN7@users.noreply.github.com>
2023-03-14 17:19:11 +0300

    Update form.php

--- z3/form.php
+++ z3/form.php
@@ -4,7 +4,13 @@
   <input name="year" placeholder="year">
   <label>
     <input name="gender" type="radio" value="man">
+    <?php
+    printf('man');
+    ?>
     <input name="gender" type="radio" value="woman">
+    <?php
+    printf('woman');
+    ?>
    </label>
   <input type="submit" value="ok" />
 </form>
