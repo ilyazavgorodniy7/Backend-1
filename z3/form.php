a9ea7d70f25f3303ac0d3107a8ce03f005210cb2
ArtN7 <112850344+ArtN7@users.noreply.github.com>
2023-03-21 10:41:24 +0300

    Add files via upload

---
+++ z3/form.php
@@ -0,0 +1,12 @@
+
+<form  method="POST" action="index.php"">
+ <link rel="stylesheet" href="style.css" />
+    <?php 
+      <label class="user-name">
+      <br>
+      printf(Имя пользователя:);
+      <input name="name" placeholder="name">
+      <br>
+      </label>
+    ?>   
+ </form>
