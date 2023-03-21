99c663e7c974e8f7d02baef8db9cf78ae659b11d
ArtN7 <112850344+ArtN7@users.noreply.github.com>
2023-03-21 21:19:09 +0300

    Update form.php

--- z3/form.php
+++ z3/form.php
@@ -1,58 +1,58 @@
 
-<form action="index.php" method="POST">
-  <p>
-  <?php
-    printf('Имя пользователя:');
-  ?>
-  </p>
-  <input name="name" placeholder="name">
-  <p>
-  <?php
-    printf('Почта:');
-  ?>
-    </p>
-  <input name="email" type="email" placeholder="email">
-  <?php
-    printf('Год рождения:');
-  ?>
-  <input name="year" placeholder="year">
-  <?php
-    printf('Пол:');
-    printf('М');
-  ?>
-  <input type="radio" name="gender" value="1">
-  <?php
-    printf('Ж');
-  ?>
-  <input type="radio" name="gender" value="2">
-  <?php
-    printf('Количество конечностей:');
-    printf('1');
-  ?>
-  <input type="radio" value="1" name="count_limb">
-  <?php
-    printf('2');
-  ?>
-  <input type="radio" value="2" name="count_limb">
-  <?php
-    printf('3');
-  ?>
-  <input type="radio" value="3" name="count_limb">
-  <?php
-    printf('Сверхспособности:');
-  ?>
-  <select name="abilities[]" multiple="multiple"> 
-    <option value="1">Бессмертие</option>
-    <option value="2">Проходить сквозь стены</option>
-    <option value="3">Левитация</option>
-  </select>
-  <?php
-    printf('Биография:');
-  ?>
-  <textarea name="biography" placeholder="about me"></textarea>
-  <?php
-    printf('С контрактом ознакомлен(а)');
-  ?>
-  <input type="checkbox" name="checked" value="on">
-  <input type="submit" value="ok" />
-</form>
+<body>
+  <form action="index.php" method="POST">
+    <label class="user-name">
+    <?php
+      printf('Имя пользователя:');
+    ?>
+    <input name="name" placeholder="name">
+    </label>
+    <?php
+      printf('Почта:');
+    ?>
+    <input name="email" type="email" placeholder="email">
+    <?php
+      printf('Год рождения:');
+    ?>
+    <input name="year" placeholder="year">
+    <?php
+      printf('Пол:');
+      printf('М');
+    ?>
+    <input type="radio" name="gender" value="1">
+    <?php
+      printf('Ж');
+    ?>
+    <input type="radio" name="gender" value="2">
+    <?php
+      printf('Количество конечностей:');
+      printf('1');
+    ?>
+    <input type="radio" value="1" name="count_limb">
+    <?php
+      printf('2');
+    ?>
+    <input type="radio" value="2" name="count_limb">
+    <?php
+      printf('3');
+    ?>
+    <input type="radio" value="3" name="count_limb">
+    <?php
+      printf('Сверхспособности:');
+    ?>
+    <select name="abilities[]" multiple="multiple"> 
+      <option value="1">Бессмертие</option>
+      <option value="2">Проходить сквозь стены</option>
+      <option value="3">Левитация</option>
+    </select>
+    <?php
+      printf('Биография:');
+    ?>
+    <textarea name="biography" placeholder="about me"></textarea>
+    <?php
+      printf('С контрактом ознакомлен(а)');
+    ?>
+    <input type="checkbox" name="checked" value="on">
+    <input type="submit" value="ok" />
+  </form>
+</body>
