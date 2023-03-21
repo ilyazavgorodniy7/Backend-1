8681726c8edd3d6d68a136ac53234d96f631642b
ArtN7 <112850344+ArtN7@users.noreply.github.com>
2023-03-21 13:57:44 +0300

    Update form.php

--- z3/form.php
+++ z3/form.php
@@ -3,9 +3,7 @@
   <?php
    echo "<link rel='stylesheet' href=/style.css>"; 
     printf('Имя пользователя:');
-  <label class="user-name">
     <input name="name" placeholder="name">
-    </label>
   ?>
   <input name="fio" />
   <input name="year" placeholder="year">
