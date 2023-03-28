c073f16ece2c45bee778ef508bc2f23c6f4c30bd
ArtN7 <112850344+ArtN7@users.noreply.github.com>
2023-03-28 08:51:38 +0300

    Update index.php

--- z4/index.php
+++ z4/index.php
@@ -1,23 +1,100 @@
 <?php
 header('Content-Type: text/html; charset=UTF-8');
-
-// В суперглобальном массиве $_SERVER PHP сохраняет некторые заголовки запроса HTTP
-// и другие сведения о клиненте и сервере, например метод текущего запроса $_SERVER['REQUEST_METHOD'].
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-  // В суперглобальном массиве $_GET PHP хранит все параметры, переданные в текущем запросе через URL.
-  if (!empty($_GET['save'])) {
-    // Если есть параметр save, то выводим сообщение пользователю.
-    print('Спасибо, результаты сохранены.');
+  $messages = array();
+  if (!empty($_COOKIE['save'])) {
+    setcookie('save', '', 100000);
+    $messages[] = 'Спасибо, результаты сохранены.';
   }
-  exit();
+  $errors = array();
+  $errors['name'] = !empty($_COOKIE['name_error']);
+  $errors['year'] = !empty($_COOKIE['year_error']);
+  $errors['email'] = !empty($_COOKIE['emal_error']);
+  $errors['gender'] = !empty($_COOKIE['gender_error']);
+  $errors['count_limb'] = !empty($_COOKIE['count_limb_error']);
+  $errors['biography '] = !empty($_COOKIE['biography_error']);
+  $errors['checked'] = !empty($_COOKIE['checked_error']);
+  $errors['abilities'] = !empty($_COOKIE['abilities_error']);
+	
+  if ($errors['name']) {
+    setcookie('name_error', '', 100000);
+    $messages[] = '<div class="error">Заполните имя.</div>';
+  }
+  $values = array();
+  $values['name'] = empty($_COOKIE['name_value']) ? '' : $_COOKIE['name_value'];
+
+  include('form.php');
 }
-include('form.php');
-// Иначе, если запрос был методом POST, т.е. нужно проверить данные и сохранить их в XML-файл.
 
 $bioreg = "/^\s*\w+[\w\s\.,-]*$/";
 $reg = "/^\w+[\w\s-]*$/";
 $mailreg = "/^[\w\.-]+@([\w-]+\.)+[\w-]{2,4}$/";
 $list_abilities = array('1','2','3');
+else {
+  $errors = FALSE;
+  if (empty($_POST['name'])) {
+    setcookie('name_error', '1', time() + 24 * 60 * 60);
+    $errors = TRUE;
+  }
+  else {
+    setcookie('name_value', $_POST['fio'], time() + 30 * 24 * 60 * 60 * 12);
+  }
+  if (empty($_POST['name'])) {
+    setcookie('name_error', '1', time() + 24 * 60 * 60);
+    $errors = TRUE;
+  }
+  else {
+    setcookie('name_value', $_POST['fio'], time() + 30 * 24 * 60 * 60 * 12);
+  }	
+
+
+  if ($errors) {
+    header('Location: index.php');
+    exit();
+  }
+  else {
+    setcookie('name_error', '', 100000);
+  }
+
+  $user = 'u52826';
+  $pass = '4927417';
+  $db = new PDO('mysql:host=localhost;dbname=u52826', $user, $pass,
+  [PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+
+  try {
+	  $stmt = $db->prepare("INSERT INTO person SET name = ?,email= ?, year= ?, gender= ?, count_limb= ?, biography= ?,checked= ?");
+	  $stmt->execute([$_POST['name'],$_POST['email'],$_POST['year'],$_POST['gender'],$_POST['count_limb'],$_POST['biography'],$_POST['checked']]);
+
+	  $id = $db->lastInsertId();
+	  $sppe= $db->prepare("INSERT INTO abilities SET power_id=:power, person_id=:person");
+	  $sppe->bindParam(':person', $id);
+	  foreach($_POST['abilities']  as $ability){
+		$sppe->bindParam(':power', $ability);
+		if($sppe->execute()==false){
+		  print_r($sppe->errorCode());
+		  print_r($sppe->errorInfo());
+		  exit();
+		}
+	  }
+  }
+  catch(PDOException $e){
+    print('Error : ' . $e->getMessage());
+    exit();
+  }
+  setcookie('save', '1');
+  header('Location: index.php');
+}
+
+
+
+
+
+
+
+
+
+
+
 
 $errors = FALSE;
 if(empty($_POST['name'])){
@@ -75,29 +152,5 @@ if ($errors) {
   exit();
 }
 
-$user = 'u52826';
-$pass = '4927417';
-$db = new PDO('mysql:host=localhost;dbname=u52826', $user, $pass,
-  [PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
 
-try {
-  $stmt = $db->prepare("INSERT INTO person SET name = ?,email= ?, year= ?, gender= ?, count_limb= ?, biography= ?,checked= ?");
-  $stmt->execute([$_POST['name'],$_POST['email'],$_POST['year'],$_POST['gender'],$_POST['count_limb'],$_POST['biography'],$_POST['checked']]);
-  
-  $id = $db->lastInsertId();
-  $sppe= $db->prepare("INSERT INTO abilities SET power_id=:power, person_id=:person");
-  $sppe->bindParam(':person', $id);
-  foreach($_POST['abilities']  as $ability){
-	$sppe->bindParam(':power', $ability);
-	if($sppe->execute()==false){
-	  print_r($sppe->errorCode());
-	  print_r($sppe->errorInfo());
-	  exit();
-	}
-  }
-}
-catch(PDOException $e){
-  print('Error : ' . $e->getMessage());
-  exit();
-}
 header('Location: ?save=1');
