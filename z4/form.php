2996dfb99b7358787a9ca630824d9c8d0ed17075
ArtN7 <112850344+ArtN7@users.noreply.github.com>
2023-03-28 16:20:09 +0300

    Update form.php

--- z4/form.php
+++ z4/form.php
@@ -28,7 +28,7 @@
         printf('Имя пользователя:');
       ?>
       <br>
-      <input name="name" placeholder="name" <?php if ($errors['name']) {print 'class="error"';} ?> name_value="<?php print $values['name'];?>" >
+      <input name="name" placeholder="name" <?php if ($errors['name']) {print 'class="error"';} ?> >
       </label>
       <label>
       <?php
