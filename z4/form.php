6cb3f1ca12b68d842345f4eedd4bd8320abffdac
ArtN7 <112850344+ArtN7@users.noreply.github.com>
2023-03-28 15:50:41 +0300

    Update form.php

--- z4/form.php
+++ z4/form.php
@@ -89,11 +89,11 @@
       <br>
       <textarea name="biography" placeholder="about me"  <?php if ($errors['biography']) {print 'class="error"';} ?> biography_value="<?php print $values['biography_limb']; ?>"></textarea>
       </label>
-      <label <?php if ($errors['checked']) {print 'class="error"';} ?> checked_value="<?php print $values['checked_limb']; ?>">
+      <label>
       <?php
         printf('С контрактом ознакомлен(-а)');
       ?>
-      <input type="checkbox" name="checked" value="on">
+      <input type="checkbox" name="checked" value="on" <?php if ($errors['checked']) {print 'class="error"';} ?> checked_value="<?php print $values['checked_limb']; ?>">
       </label>
       <label>
       <input type="submit" value="ok" class="button"/>
