9bdd326dce362b301d96802d0b8be1b92216ce31
ArtN7 <112850344+ArtN7@users.noreply.github.com>
2023-04-04 14:28:11 +0300

    Update login.php

--- z5/login.php
+++ z5/login.php
@@ -17,7 +17,7 @@ session_start();
 
 // В суперглобальном массиве $_SESSION хранятся переменные сессии.
 // Будем сохранять туда логин после успешной авторизации.
-if (!empty($_SESSION['user_id'])) {
+if (!empty($_SESSION['login'])) {
   // Если есть логин в сессии, то пользователь уже авторизован.
   // TODO: Сделать выход (окончание сессии вызовом session_destroy()
   //при нажатии на кнопку Выход).
@@ -31,7 +31,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
 ?>
 
 <form action="index.php" method="post">
-  <input name="user_id" placeholder="user_id"/>
+  <input name="login" placeholder="login"/>
   <input name="password"  placeholder="password"/>
   <input type="submit" value="Войти" />
 </form>
@@ -43,9 +43,9 @@ else {
   // TODO: Проверть есть ли такой логин и пароль в базе данных.
   // Выдать сообщение об ошибках.
   // Если все ок, то авторизуем пользователя.
-  $_SESSION['user_id'] = $_POST['user_id'];
+  $_SESSION['login'] = $_POST['login'];
   // Записываем ID пользователя.
-  $_SESSION['id'] = $db->lastInsertId();;
+  $_SESSION['user_id'] = $db->lastInsertId();;
 
   // Делаем перенаправление.
   header('Location: ./');
