d657e12a088e06c053f69d22d53bbd0392dfda8d
ArtN7 <112850344+ArtN7@users.noreply.github.com>
2023-04-04 14:34:53 +0300

    Update login.php

--- z5/login.php
+++ z5/login.php
@@ -19,7 +19,7 @@ if (!empty($_SESSION['login'])) {
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
 ?>
 
-<form action="index.php" method="POST">
+<form action="index.php" method="POST" width="150px">
   <input name="login" placeholder="login"/>
   <input name="password"  placeholder="password"/>
   <input type="submit" value="Войти" />
